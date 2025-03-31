e381a249f6fea3f49b358ec131308d642697c572
sethumvidmal <sethumthabrew@gmail.com>
2025-03-31 09:21:26 +0530

    url change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-const BASE_URL = "http://api.weatherapi.com/v1";
+const BASE_URL = "https://api.weatherapi.com/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
